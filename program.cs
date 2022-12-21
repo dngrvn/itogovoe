250c153b47910fddf4314981f69452e55b42b50a
dngrvn <108631571+dngrvn@users.noreply.github.com>
2022-12-22 01:25:13 +0300

    Create program.cs
    внесено изменение

--- program.cs
+++ program.cs
@@ -7,7 +7,7 @@ string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {int count = 0;
     for (int i = 0; i < array1.Length; i++)
-    {if(array1[i].Length <= 3)
+    {if(array1[i].Length <= 4)
         {array2[count] = array1[i];
         count++;}}}
 void PrintArray(string[] array)
